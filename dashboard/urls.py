35b64494c4ee5ac3ca1171f79fb4d9cc447fbbf9
Amara Datta <amaradatta@gmail.com>
2018-03-31 13:58:53 -0700

    add table to html to prettify stock data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,5 +8,4 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.obtain_latest_data, name='obtain_latest_data'),
     path('add/', TemplateView.as_view(template_name='add_stock.html'), name='add_stock'),
-    # path('', utils.get_all, name='get_all'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -10,13 +10,18 @@ def obtain_latest_data(request):
     parsed_data = []
     for stock in stocks:
         tckr = stock.company_ticker_name
+        name = stock.company_name
         json_data = raw_data(tckr)
         meta_data = {}
         date = json_data['Meta Data']['3. Last Refreshed']
-        price_data = json_data['Time Series (60min)'][date]
         meta_data['price'] = {}
+        price_data = json_data['Time Series (60min)'][date]
         meta_data['price']['open'] = price_data['1. open']
+        meta_data['price']['high'] = price_data['2. high']
+        meta_data['price']['low'] = price_data['3. low']
+        meta_data['price']['close'] = price_data['4. close']
         meta_data['date'] = date
         meta_data['ticker'] = tckr
+        meta_data['name'] = name
         parsed_data.append(meta_data)
     return render(request, 'dashboard.html', {'parsed_data': parsed_data})
