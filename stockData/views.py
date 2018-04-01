814c53c1c6a62c503585af222d6e24b79ff262be
Amara Datta <amaradatta@gmail.com>
2018-03-31 21:43:26 -0700

    Add base template

--- stockData/views.py
+++ stockData/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .forms import StockForm
+from .forms import StockForm, DeleteForm
 from .models import StockData
 
 
@@ -27,7 +27,7 @@ def add_user_ticker(request):
 @login_required
 def delete_user_ticker(request):
     if request.method == 'POST':
-        stock_form = StockForm(request.POST)
+        stock_form = DeleteForm(request.POST)
 
         if stock_form.is_valid():
             ticker_name = stock_form.cleaned_data['ticker_name']
