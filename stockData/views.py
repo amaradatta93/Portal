36ee71d2362891b023915559fb0b17b443ffe184
Amara Datta <amaradatta@gmail.com>
2018-03-31 17:08:11 -0700

    fix login screen

--- stockData/views.py
+++ stockData/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import StockForm
