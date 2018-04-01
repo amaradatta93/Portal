5a34f00a65967ec3e5c83b28cb1c1b57241b6834
Amara Datta <amaradatta@gmail.com>
2018-03-31 22:44:08 -0700

    Remove the toggle bar

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
-from . import views, utils
+from . import views
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.obtain_latest_data, name='obtain_latest_data'),
-    path('add/', TemplateView.as_view(template_name='add_stock.html'), name='add_stock'),
 ]
